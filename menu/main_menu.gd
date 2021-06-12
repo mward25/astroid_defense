9d3bfc1a7502e8594fa411e258c28b9aa1bac7d2
miles <milesward2004@gmail.com>
2021-06-12 13:57:30 -0700

    changed more network stuff.

--- menu/main_menu.gd
+++ menu/main_menu.gd
@@ -8,26 +8,32 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	get_tree().connect("network_peer_connected", self, "_player_connected")
+	get_tree().connect("network_peer_disconnected", self, "_player_disconnected")
+	get_tree().connect("connected_to_server", self, "_connected_ok")
+	get_tree().connect("connection_failed", self, "_connected_fail")
+	get_tree().connect("server_disconnected", self, "_server_disconnected")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
-
+var playerInfo = {}
+var myInfo = {name = "the dude"}
 
 func _on_Button_pressed():
 		if $EnterIp/Ip.text == "":
 			var peer = NetworkedMultiplayerENet.new()
 			peer.create_server(2738, 10)
 			get_tree().network_peer = peer
-			pre_configure_game()
 		else:
 			var peer = NetworkedMultiplayerENet.new()
 			peer.create_client($EnterIp/Ip.text, $EnterServerPort/Port.text)
 			get_tree().network_peer = peer
-			pre_configure_game()
 
+
+func update_ui(var info):
+	$Names/RichTextLabel.text = playerInfo
 
 remote func pre_configure_game():
 	var selfPeerID = get_tree().get_network_unique_id()
@@ -41,7 +47,29 @@ remote func pre_configure_game():
 	MyPlayer.set_network_master(selfPeerID)
 	get_node("/root/world/players").add_child(MyPlayer)
 	
+	for p in playerInfo:
+		var player = preload("res://player/player_default.tscn")
+		player.set_name(str(p))
+		player.set_network_master(p)
+		get_node("/root/world/players").add_child(player)
 	
+	rpc_id(1, "done_preconfiguring")
+
+func _player_connected(id):
+	rpc_id(id, "register_player", myInfo)
+
+func _player_disconnected(id):
+	playerInfo.erase(id)
+
+func _connected_ok():
+	pass
+
+func _connected_fail():
+	pass
+
+remote func register_player(info):
+	var id = get_tree().get_rpc_sender_id()
+	playerInfo[id] = info
 	
-	
-	
+	update_ui(str(playerInfo))
+
