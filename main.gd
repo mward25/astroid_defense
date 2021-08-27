36063d2ccf47d63a9a1ae9c5cb8f766e9fc961f2
miles <milesward2004@gmail.com>
2021-08-27 13:43:00 -0700

    added debugging statement

--- main.gd
+++ main.gd
@@ -10,6 +10,7 @@ var mainMenu = preload("res://menu/main_menu.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
+	print("starting astroid defence")
 	if "--server" in OS.get_cmdline_args() || OS.has_feature("Server"):
 		print("running as server")
 		Network.isHeadless = true
