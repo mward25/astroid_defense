4686e2d10e306639a882a3c13a989f1f56bc0c9f
miles <milesward2004@gmail.com>
2021-09-23 14:51:52 -0700

    added debugging stuff

--- do_git_things.sh
+++ do_git_things.sh
@@ -1,6 +1,8 @@
 echo "starting git things"
 cd ~/.local/share/godot/app_userdata/Astroid\ Defence/
 git add .
-"added git things"
+echo "added git things"
+echo "pwd is "
+pwd
 git commit -m updated_save_dict
-"commited git things"
+echo "commited git things"
